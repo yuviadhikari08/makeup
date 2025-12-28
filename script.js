355875be4a2592097a69ab287d49fc4ead9dead0
yuviadhikari08 <yuviadhikari08@gmail.com>
2025-12-28 19:11:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openLightbox(img) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
